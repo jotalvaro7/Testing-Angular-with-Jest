56437645f5a14c2ac20170418841ca02ab277385
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-05-23 15:06:25 -0500

    implementando comunicacion con padre hijo y generacion de eventos

--- src/app/shared/father-son/components/father-son/father-son.component.ts
+++ src/app/shared/father-son/components/father-son/father-son.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Client } from '@father/shared/model/client';
 
 @Component({
@@ -9,6 +9,22 @@ import { Client } from '@father/shared/model/client';
 export class FatherSonComponent{
 
   @Input() client?: Client;
+  @Output() onDeleteClient = new EventEmitter();
+  @Output() onClientUpdate = new EventEmitter<Client>();
+
+  onDelete(){
+    this.client = undefined;
+    this.onDeleteClient.emit();
+  }
+
+  onChange(id: number){
+    if(!this.client) return
+    
+    /* this.client.id = newId; */ //No usar esto
+
+    this.client = {...this.client, id}
+    this.onClientUpdate.emit({...this.client});
+  }
   
 
 }
